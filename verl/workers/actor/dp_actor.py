727d7f96d7634c3f18a8e4d28310fd96e2a6cc60
nanjiangwill <willjiang2018@gmail.com>
2025-06-26 16:11:09 -0700

    fix image type

--- verl/workers/actor/dp_actor.py
+++ verl/workers/actor/dp_actor.py
@@ -89,7 +89,13 @@ class DataParallelPPOActor(BasePPOActor):
         multi_modal_inputs = {}
         if "multi_modal_inputs" in micro_batch.keys():
             for key in micro_batch["multi_modal_inputs"][0].keys():
-                multi_modal_inputs[key] = torch.cat([inputs[key] for inputs in micro_batch["multi_modal_inputs"]], dim=0)
+                inputs_list = [inputs[key] for inputs in micro_batch["multi_modal_inputs"]]
+
+                # when rollout generates new multi_modal_inputs, the inputs_list is a list, not a tensor
+                if isinstance(inputs_list[0], list):
+                    inputs_list = [torch.tensor(x, device=get_device_id()) for x in inputs_list]
+
+                multi_modal_inputs[key] = torch.cat(inputs_list, dim=0)
 
         with torch.autocast(device_type=self.device_name, dtype=torch.bfloat16):
             input_ids = micro_batch["input_ids"]
